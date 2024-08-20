9df5e275e765dde747250649afc1a5addd9f051d
student leepakshikale <leepakshikale@gmail.com>
2024-08-20 00:35:00 -0700

    adding files

---
+++ patterns/pattern8.java
@@ -0,0 +1,41 @@
+//print -> rhombus pattern
+       
+    //             * * * * *
+    //           * * * * *
+    //         * * * * *
+    //       * * * * * 
+   //      * * * * * 
+
+
+
+
+
+
+
+public class pattern8 {
+    public static void main(String args[]){ 
+
+    int n=5;
+       
+         for (int i=1; i<=n; i++){
+            //first part
+             //spaces
+             for(int j=1; j<=n-i; j++){
+                System.out.print("  ");
+             }
+              //second part-> stars
+             for (int j=1; j<=5; j++) {
+                System.out.print(" *");  
+             }
+             
+            //  for(int j=1; j<=i; j++){
+            //     System.out.print("* "); 
+            //  }
+             System.out.println();
+                
+         }
+
+        
+        }
+    
+}
