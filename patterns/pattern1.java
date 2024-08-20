65f847823dc522347365f27579f6f926a4e10ab2
student leepakshikale <leepakshikale@gmail.com>
2024-08-19 20:41:11 -0700

    add file

--- patterns/pattern1.java
+++ patterns/pattern1.java
@@ -12,7 +12,7 @@
 
     
 
-public class patterns1 {
+public class pattern1 {
   
     public static void main(String args[]){
 
--- patterns/pattern3.java
+++ patterns/pattern3.java
@@ -7,24 +7,18 @@
    
 
 
-
-
-
-public class patterns2 {
-   
+public class pattern3 {
+    
     public static void main(String args[]){
 
-        int n=4;
-        int m=5;
+       
 
-            for (int i=1; i<=n; i++){
-                for (int j=1; j<=m; j++) {
-                    if (i==1 || j==1 || i==n || j==m){
+            for (int i=4; i>=1; i--){
+                for (int j=1; j<=i; j++) {
+                  
                         System.out.print("* ");
                    
-                    }else{
-                        System.out.print("  ");
-                    }
+                  
                 }
                    
                     System.out.println();
@@ -34,8 +28,5 @@ public class patterns2 {
                
             }
           
- 
-        }
-
     
-
+}
